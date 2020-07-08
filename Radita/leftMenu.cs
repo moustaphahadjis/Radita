c1831be155e314f3fee164a619e4a0c854acc40d
Tapha Hadji <Tapha Hadji@DESKTOP-QIA7BNC>
2020-07-07 21:15:38 -0700

    Add project files.

---
+++ Radita/leftMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radita
+{
+    public partial class leftMenu : Form
+    {
+        public leftMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
